d7d29d353b1cbe08ae65105d0ca25d73cd2e032a
erogenousbeef <erogenousbeef@gmail.com>
2013-06-11 17:31:01 +0200

    REACTORS FIXED: Adding MB Event Handler
    It mostly works now, just needs testing.

--- erogenousbeef/bigreactors/common/BRLoader.java
+++ erogenousbeef/bigreactors/common/BRLoader.java
@@ -3,6 +3,7 @@ package erogenousbeef.bigreactors.common;
 import net.minecraftforge.common.MinecraftForge;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.ServerAboutToStart;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.Mod.Init;
 import cpw.mods.fml.common.Mod.Instance;
@@ -10,12 +11,16 @@ import cpw.mods.fml.common.Mod.PreInit;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerAboutToStartEvent;
 import cpw.mods.fml.common.network.NetworkMod;
 import cpw.mods.fml.common.network.NetworkMod.SidedPacketHandler;
 import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import erogenousbeef.bigreactors.client.ClientPacketHandler;
 import erogenousbeef.bigreactors.net.ServerPacketHandler;
 import erogenousbeef.bigreactors.net.ConnectionHandler;
+import erogenousbeef.core.multiblock.MultiblockEventHandler;
 
 
 @Mod(modid = BigReactors.CHANNEL, name = BigReactors.NAME, version = BRConfig.VERSION, acceptedMinecraftVersions = "[1.5,)")
@@ -29,6 +34,8 @@ public class BRLoader {
 
 	@SidedProxy(clientSide = "erogenousbeef.bigreactors.client.ClientProxy", serverSide = "erogenousbeef.bigreactors.common.CommonProxy")
 	public static CommonProxy proxy;
+	
+	private MultiblockEventHandler multiblockEventHandler;
 	
 	@PreInit
 	public void preInit(FMLPreInitializationEvent event)
@@ -56,5 +63,11 @@ public class BRLoader {
 	@PostInit
 	public void postInit(FMLPostInitializationEvent evt) {
 		proxy.postInit();
+	}
+	
+	@ServerAboutToStart
+	public void serverAboutToStart(FMLServerAboutToStartEvent evt) {
+		multiblockEventHandler = new MultiblockEventHandler();
+		MinecraftForge.EVENT_BUS.register(multiblockEventHandler);
 	}
 }
