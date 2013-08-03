4be8fe24d28f2ed501c17744ecce9ceffc7e8c1e
erogenousbeef <erogenousbeef@gmail.com>
2013-08-03 21:12:38 +0200

    Control rod selection UI
    In RedNet config GUI, you can now select control rods for setting types
    that are nonglobal.

--- erogenousbeef/bigreactors/gui/controls/grab/RedNetConfigGrabTarget.java
+++ erogenousbeef/bigreactors/gui/controls/grab/RedNetConfigGrabTarget.java
@@ -1,6 +1,7 @@
 package erogenousbeef.bigreactors.gui.controls.grab;
 
 import erogenousbeef.bigreactors.client.gui.BeefGuiBase;
+import erogenousbeef.bigreactors.client.gui.GuiReactorRedNetPort;
 import erogenousbeef.bigreactors.common.tileentity.TileEntityReactorRedNetPort;
 import erogenousbeef.bigreactors.gui.IBeefTooltipControl;
 
@@ -23,12 +24,20 @@ public class RedNetConfigGrabTarget extends BeefGuiGrabTarget implements IBeefTo
 	public void onSlotCleared() {
 		currentCircuitType = TileEntityReactorRedNetPort.CircuitType.DISABLED;
 		tooltip = null;
+		
+		if(guiContainer instanceof GuiReactorRedNetPort) {
+			((GuiReactorRedNetPort)guiContainer).onChannelChanged(this.channel);
+		}
 	}
 
 	@Override
 	public void onSlotSet() {
 		currentCircuitType = ((RedNetConfigGrabbable)this.grabbable).GetCircuitType();
 		tooltip = ((RedNetConfigGrabbable)this.grabbable).getName();
+		
+		if(guiContainer instanceof GuiReactorRedNetPort) {
+			((GuiReactorRedNetPort)guiContainer).onChannelChanged(this.channel);
+		}		
 	}
 
 	@Override
