f1616dc3b1ec488b5dd960fab777a7696f5bdbbb
erogenousbeef <erogenousbeef@gmail.com>
2014-01-25 21:58:56 +0100

    (BROKEN) Corifying fuelcontainer, adding coolantcontainer

---
+++ erogenousbeef/bigreactors/common/multiblock/helpers/CoolantContainer.java
@@ -0,0 +1,15 @@
+package erogenousbeef.bigreactors.common.multiblock.helpers;
+
+import net.minecraftforge.fluids.FluidStack;
+
+public class CoolantContainer extends FluidHelper {
+
+	private static final int TANK_COLD = 0;
+	private static final int TANK_HOT = 1;
+
+	private float rfAbsorbed;
+	
+	public CoolantContainer() {
+	}
+
+}
