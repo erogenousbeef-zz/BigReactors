6bb1daf6dded83f84a9f7214901c293d32a14a3e
erogenousbeef <erogenousbeef@gmail.com>
2014-06-03 22:22:59 +0200

    Fix #183: Creative coolant inlets now fill entire coolant tank each tick

--- erogenousbeef/bigreactors/common/multiblock/tileentity/creative/TileEntityReactorCreativeCoolantPort.java
+++ erogenousbeef/bigreactors/common/multiblock/tileentity/creative/TileEntityReactorCreativeCoolantPort.java
@@ -3,6 +3,7 @@ package erogenousbeef.bigreactors.common.multiblock.tileentity.creative;
 import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import erogenousbeef.bigreactors.common.multiblock.MultiblockReactor;
+import erogenousbeef.bigreactors.common.multiblock.helpers.CoolantContainer;
 import erogenousbeef.bigreactors.common.multiblock.interfaces.ITickableMultiblockPart;
 import erogenousbeef.bigreactors.common.multiblock.tileentity.TileEntityReactorCoolantPort;
 
@@ -19,13 +20,17 @@ public class TileEntityReactorCreativeCoolantPort extends TileEntityReactorCoola
 		MultiblockReactor reactor = getReactorController();
 
 		if(isInlet()) {
-			reactor.getCoolantContainer().addCoolant(new FluidStack(FluidRegistry.WATER, 100));
+			CoolantContainer cc = reactor.getCoolantContainer();
+			if(cc.getCoolantAmount() < cc.getCapacity())
+			{
+				reactor.getCoolantContainer().addCoolant(new FluidStack(FluidRegistry.WATER, cc.getCapacity()));
+			}
 		}
 		else {
 			reactor.getCoolantContainer().emptyVapor();
 		}
 	}
-	
+
 	public void forceAddWater() {
 		if(!isConnected()) { return; }
 		
