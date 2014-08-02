c550e0f1440f90b42bb9f089db058e3913a82d99
erogenousbeef <erogenousbeef@gmail.com>
2014-08-02 20:43:06 +0200

    Additional deleted files from the merge

--- erogenousbeef/bigreactors/common/data/RadiationPacket.java
+++
@@ -1,6 +0,0 @@
-package erogenousbeef.bigreactors.common.data;
-
-public class RadiationPacket {
-	public float hardness;
-	public float intensity;
-}
--- erogenousbeef/bigreactors/gui/IBeefTooltipControl.java
+++
@@ -1,9 +0,0 @@
-package erogenousbeef.bigreactors.gui;
-
-
-public interface IBeefTooltipControl {
-	boolean isMouseOver(int mouseX, int mouseY);
-	String[] getTooltip();
-	
-	public boolean isVisible();
-}
