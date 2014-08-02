fcf656479100072f7ef8713e75db68a537aa378d
erogenousbeef <erogenousbeef@gmail.com>
2014-08-02 20:43:06 +0200

    Additional deleted files from the merge

--- erogenousbeef/bigreactors/api/IRadiationModerator.java
+++
@@ -1,8 +0,0 @@
-package erogenousbeef.bigreactors.api;
-
-import erogenousbeef.bigreactors.common.data.RadiationData;
-import erogenousbeef.bigreactors.common.data.RadiationPacket;
-
-public interface IRadiationModerator {
-	public void moderateRadiation(RadiationData returnData, RadiationPacket radiation);
-}
