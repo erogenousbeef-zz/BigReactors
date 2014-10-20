550d04152675af21733b300dc871a9d75faef957
Mr. E. Beef <erogenousbeef@gmail.com>
2014-10-21 00:01:57 +0200

    Add registry for exponential steams and add crude SteamEvapContainer for use by the heat exchanger

--- src/main/java/erogenousbeef/bigreactors/common/data/StandardFluids.java
+++ src/main/java/erogenousbeef/bigreactors/common/data/StandardFluids.java
@@ -4,11 +4,16 @@ import net.minecraftforge.fluids.FluidRegistry;
 import net.minecraftforge.fluids.FluidStack;
 import erogenousbeef.bigreactors.api.data.FluidStateData;
 import erogenousbeef.bigreactors.api.registry.FluidStates;
+import erogenousbeef.bigreactors.api.registry.SteamRegistry;
 import erogenousbeef.bigreactors.common.BigReactors;
 
 public class StandardFluids {
 
 	private static boolean m_Registered = false;
+	
+	public static final int BoilingPoint_Water = 100;
+	public static final int Rf_Water = 4;
+	public static final int Expansion_Water = 1; // TODO: Change to 100 or something, so 1 unit water >> 100 units steam
 	
 	public static void register() {
 		if(m_Registered) { return; }
@@ -16,12 +21,13 @@ public class StandardFluids {
 		FluidStates.registerData(new FluidStateData("water",
 			FluidRegistry.getFluid("water"),
 			BigReactors.fluidSteam,
-			100,// 100C boiling point for water
-			4,	// 1mB water<>steam = 4 rf
-			1	// TODO: Change to 100 or something, so 1 unit water >> 100 units steam
+			BoilingPoint_Water,	
+			Rf_Water,
+			Expansion_Water		
 		));
+		
+		SteamRegistry.setDefault(BigReactors.fluidSteam);
 
 		m_Registered = true;
 	}
-	
 }
